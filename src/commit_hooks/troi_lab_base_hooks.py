6ab08a025b7c3dc862eeb8deba44ad6c16e359ba
Aju George <Aju.George@uk-erlangen.de>
2026-03-16 14:35:19 +0100

    refactor: replace os.execvp with subprocess

--- src/commit_hooks/troi_lab_base_hooks.py
+++ src/commit_hooks/troi_lab_base_hooks.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 import os
 import sys
-
+import subprocess
 
 def troi_lab_base_hooks():
     config_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "templates","configs")
     cfg = os.path.join(config_path, ".pre-commit-config.yaml")
     cmd = ["pre-commit", "run", "--config", cfg, "--files"] + sys.argv[1:]
-    os.execvp(cmd[0], cmd)
-    sys.exit(0)
+    result = subprocess.run(cmd)
+    sys.exit(result.returncode)
