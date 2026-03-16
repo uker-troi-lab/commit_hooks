40cc5f19ef09453dc5d67ffb46a9ab3885607064
Aju George <Aju.George@uk-erlangen.de>
2026-03-16 14:06:50 +0100

    refactor: moved base pre-commit config to templates/configs

--- src/commit_hooks/troi_lab_base_hooks.py
+++ src/commit_hooks/troi_lab_base_hooks.py
@@ -2,15 +2,10 @@
 import os
 import sys
 
-HERE = os.path.dirname(os.path.realpath(__file__))
-
 
 def troi_lab_base_hooks():
-    cfg = os.path.join(HERE, ".pre-commit-config.yaml")
-    cmd = ['pre-commit', 'run', '--config', cfg, '--files'] + sys.argv[1:]
+    root_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "templates","configs")
+    cfg = os.path.join(root_path, ".pre-commit-config.yaml")
+    cmd = ["pre-commit", "run", "--config", cfg, "--files"] + sys.argv[1:]
     os.execvp(cmd[0], cmd)
     sys.exit(0)
-
-
-# if __name__ == '__main__':
-#     exit(main())
