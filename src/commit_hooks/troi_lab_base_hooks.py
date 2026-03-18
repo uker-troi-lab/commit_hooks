a80b4fb8d1f97c635fc31524a9e988c2b0b688b4
kapsner <lorenz.kapsner@gmail.com>
2026-03-18 08:47:15 +0000

    chore: unhide cfg file

--- src/commit_hooks/troi_lab_base_hooks.py
+++ src/commit_hooks/troi_lab_base_hooks.py
@@ -4,8 +4,13 @@ import sys
 import subprocess
 
 def troi_lab_base_hooks():
-    config_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "templates","configs")
-    cfg = os.path.join(config_path, ".pre-commit-cfg.yaml")
+    config_path = os.path.join(
+      os.path.dirname(
+        os.path.realpath(__file__)),
+        "templates", 
+        "configs",
+    )
+    cfg = os.path.join(config_path, "pre-commit-cfg.yaml")
     cmd = ["pre-commit", "run", "--config", cfg, "--files"] + sys.argv[1:]
     result = subprocess.run(cmd)
     sys.exit(result.returncode)
